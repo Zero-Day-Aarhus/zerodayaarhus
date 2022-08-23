5904d3e489762d1f2884ed516c6f63d5aba4ac9f
Morten Raeburn <morten@raeburn.dev>
2022-08-23 13:57:16 +0200

    Update file inclusion

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir ./docs
 
 #Declare a string array
-FileArray=("index.html"  "logo.png")
+FileArray=("index.html" "logo.png" "logo_long.png" "key.pub")
  
 for file in ${FileArray[*]}; do
      cp $file ./docs/
