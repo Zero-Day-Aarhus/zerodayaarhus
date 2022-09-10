51dd26c5270d2492ee3429273f3b9a4acef5b3fa
Joakim5005 <fherach@gmail.com>
2022-09-10 21:45:19 +0200

    Add CEO

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir ./docs
 
 #Declare a string array
-FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME")
+FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME" "googled49ee5ba9d18fa74.html" "robots.txt")
  
 for file in ${FileArray[*]}; do
      cp $file ./docs/
