5102b1019b009b28a760bffdd7a0a558490af492
Joakim5005 <fherach@gmail.com>
2023-02-02 19:44:00 +0100

    Small fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir ./docs
 
 #Declare a string array
-FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME" "googled49ee5ba9d18fa74.html" "wallet1.png" "wallet2.png" "et-nul.png" )
+FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME" "googled49ee5ba9d18fa74.html" "wallet1.png" "wallet2.png" "et-nul.png" "writeups.html")
  
 for file in ${FileArray[*]}; do
      cp $file ./docs/
